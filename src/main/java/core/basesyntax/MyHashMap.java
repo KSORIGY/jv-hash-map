08d62ab98f2bd2f2fb0dfbd633cb5c4b24b2bfc9
DenysHirniak <denforever777@gmail.com>
2025-10-20 20:32:44 +0300

    solution of my hashMap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
-
     private Node<K, V>[] table;
     private int size;
     private int capacity;
